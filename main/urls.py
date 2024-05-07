b13b0f29368f299460d2a1155ee0490a0b18a421
muzna <veeshan664@gmail.com>
2024-05-07 13:27:13 +0530

    Add Files

---
+++ main/urls.py
@@ -0,0 +1,91 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    
+    #Home
+    path('directorMessage', views.directorMessage, name='directorMessage'),
+    path('about', views.about, name='about'),
+
+#######################################################################################################################################
+
+    path('management_gazetted', views.management_gazetted, name='management_gazetted'),
+    path('management_ngSignal', views.management_ngSignal, name='management_ngSignal'),
+    path('management_ngTelecom', views.management_ngTelecom, name='management_ngTelecom'),
+
+#######################################################################################################################################
+    
+    path('department_signal', views.department_signal, name='department_signal'),
+    path('department_telecom', views.department_telecom, name='department_telecom'),
+    path('department_it', views.department_it, name='department_it'),
+    path('department_coeKavach', views.department_coeKavach, name='department_coeKavach'),
+
+#######################################################################################################################################
+
+    path('infrastructure_lecture', views.infrastructure_lecture, name='infrastructure_lecture'),
+    path('infrastructure_lecture_classrooms', views.infrastructure_lecture_classrooms, name='infrastructure_lecture_classrooms'),
+    path('infrastructure_lecture_digital_classrooms', views.infrastructure_lecture_digital_classrooms, name='infrastructure_lecture_digital_classrooms'),
+
+#######################################################################################################################################
+
+    path('infrastructure_auditorium', views.infrastructure_auditorium, name='infrastructure_auditorium'),
+    path('infrastructure_auditorium_indoor', views.infrastructure_auditorium_indoor, name='infrastructure_auditorium_indoor'),
+    path('infrastructure_auditorium_outdoor', views.infrastructure_auditorium_outdoor, name='infrastructure_auditorium_outdoor'),
+
+#######################################################################################################################################
+
+    path('infrastructure_hostels', views.infrastructure_hostels, name='infrastructure_hostels'),
+    path('infrastructure_hostels_kaveri', views.infrastructure_hostels_kaveri, name='infrastructure_hostels_kaveri'),
+    path('infrastructure_hostels_yamuna', views.infrastructure_hostels_yamuna, name='infrastructure_hostels_yamuna'),
+    path('infrastructure_hostels_krishna', views.infrastructure_hostels_krishna, name='infrastructure_hostels_krishna'),
+    path('infrastructure_hostels_ganga', views.infrastructure_hostels_ganga, name='infrastructure_hostels_ganga'),
+
+#######################################################################################################################################
+
+    path('infrastructure_sports', views.infrastructure_sports, name='infrastructure_sports'),
+    path('infrastructure_sports_indoor', views.infrastructure_sports_indoor, name='infrastructure_sports_indoor'),
+    path('infrastructure_sports_outdoor', views.infrastructure_sports_outdoor, name='infrastructure_sports_outdoor'),
+
+#######################################################################################################################################
+
+    path('events', views.events, name='events'),
+    path('annualReport', views.annualReport, name='annualReport'),
+
+    #TraineeResources
+    path('briefing', views.briefing, name='briefing'),
+    path('courseCalender', views.courseCalender, name='courseCalender'),
+    path('traineeManagement', views.traineeManagement, name='traineeManagement'),
+    path('contentManagement', views.contentManagement, name='contentManagement'),
+    path('libraryAccess', views.libraryAccess, name='libraryAccess'),
+    path('onlineExam', views.onlineExam, name='onlineExam'),
+    path('discussionForm', views.discussionForm, name='discussionForm'),
+
+    #StaffResources
+    path('darpan', views.darpan, name='darpan'),
+    path('gyandeep', views.gyandeep, name='gyandeep'),
+    path('sop', views.sop, name='sop'),
+    
+    #research
+    path('coeKavach', views.coeKavach, name='coeKavach'),
+    path('_5g', views._5g, name='_5g'),
+    path('faculty', views.faculty, name='faculty'),
+    path('mous', views.mous, name='mous'),
+    path('innovations', views.innovations, name='innovations'),
+
+    #notification
+    path('selections', views.selections, name='selections'),
+    path('notifications', views.notifications, name='notifications'),
+    path('railwaySites', views.railwaySites, name='railwaySites'),
+
+    #contacreachingt
+    path('reaching', views.reaching, name='reaching'),
+    path('telephone', views.telephone, name='telephone'),
+    path('disclaimer', views.disclaimer, name='disclaimer'),
+    path('privacyPolicy', views.privacyPolicy, name='privacyPolicy'),
+    path('sexualHarassment', views.sexualHarassment, name='sexualHarassment'),
+    path('rti', views.rti, name='rti'),
+    path('nonRailway', views.nonRailway, name='nonRailway'),
+    path('links', views.links, name='links'),
+
+]
---
+++ main/views.py
@@ -0,0 +1,241 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+#Home
+
+def directorMessage(request):
+    template = loader.get_template('home/directorMessage.html')
+    return HttpResponse(template.render())
+
+def about(request):
+    template = loader.get_template('home/about.html')
+    return HttpResponse(template.render())
+
+#######################################################################################################################################
+
+def management_gazetted(request):
+    return render(request, 'home/management/gazetted.html')
+
+
+def management_ngSignal(request):
+    template = loader.get_template('home/management/ngSignal.html')
+    return HttpResponse(template.render())
+
+def management_ngTelecom(request):
+    template = loader.get_template('home/management/ngTelecom.html')
+    return HttpResponse(template.render())
+
+#######################################################################################################################################
+
+def department_signal(request):
+    template = loader.get_template('home/department/signal.html')
+    return HttpResponse(template.render())
+
+
+def department_it(request):
+    template = loader.get_template('home/department/it.html')
+    return HttpResponse(template.render())
+
+
+def department_telecom(request):
+    template = loader.get_template('home/department/telecom.html')
+    return HttpResponse(template.render())
+
+
+def department_coeKavach(request):
+    template = loader.get_template('home/department/coeKavach.html')
+    return HttpResponse(template.render())
+
+#######################################################################################################################################
+
+def infrastructure_lecture(request):
+    template = loader.get_template('home/infrastructure/lecture.html')
+    return HttpResponse(template.render())
+
+def infrastructure_lecture_classrooms(request):
+    template = loader.get_template('home/infrastructure/lecture.html')
+    return HttpResponse(template.render())
+
+def infrastructure_lecture_digital_classrooms(request):
+    template = loader.get_template('home/infrastructure/lecture.html')
+    return HttpResponse(template.render())
+
+#######################################################################################################################################
+
+def infrastructure_auditorium(request):
+    template = loader.get_template('home/infrastructure/auditorium.html')
+    return HttpResponse(template.render())
+
+def infrastructure_auditorium_indoor(request):
+    template = loader.get_template('home/infrastructure/auditorium.html')
+    return HttpResponse(template.render())
+
+def infrastructure_auditorium_outdoor(request):
+    template = loader.get_template('home/infrastructure/auditorium.html')
+    return HttpResponse(template.render())
+
+#######################################################################################################################################
+
+def infrastructure_hostels(request):
+    template = loader.get_template('home/infrastructure/hostels.html')
+    return HttpResponse(template.render())
+
+def infrastructure_hostels_kaveri(request):
+    template = loader.get_template('home/infrastructure/hostels.html')
+    return HttpResponse(template.render())
+
+def infrastructure_hostels_yamuna(request):
+    template = loader.get_template('home/infrastructure/hostels.html')
+    return HttpResponse(template.render())
+
+def infrastructure_hostels_krishna(request):
+    template = loader.get_template('home/infrastructure/hostels.html')
+    return HttpResponse(template.render())
+
+def infrastructure_hostels_ganga(request):
+    template = loader.get_template('home/infrastructure/hostels.html')
+    return HttpResponse(template.render())
+
+#######################################################################################################################################
+
+def infrastructure_sports(request):
+    template = loader.get_template('home/infrastructure/sports.html')
+    return HttpResponse(template.render())
+
+def infrastructure_sports_indoor(request):
+    template = loader.get_template('home/infrastructure/sports.html')
+    return HttpResponse(template.render())
+
+def infrastructure_sports_outdoor(request):
+    template = loader.get_template('home/infrastructure/sports.html')
+    return HttpResponse(template.render())
+
+#######################################################################################################################################
+
+def events(request):
+    template = loader.get_template('home/events.html')
+    return HttpResponse(template.render())
+
+def annualReport(request):
+    template = loader.get_template('home/annualReport.html')
+    return HttpResponse(template.render())
+
+
+# trainee resources
+
+def briefing(request):
+    template = loader.get_template('trainee/briefing.html')
+    return HttpResponse(template.render())
+
+def courseCalender(request):
+    template = loader.get_template('trainee/courseCalender.html')
+    return HttpResponse(template.render())
+
+def traineeManagement(request):
+    template = loader.get_template('trainee/traineeManagement.html')
+    return HttpResponse(template.render())
+
+def contentManagement(request):
+    template = loader.get_template('trainee/contentManagement.html')
+    return HttpResponse(template.render())
+
+def libraryAccess(request):
+    template = loader.get_template('trainee/libraryAccess.html')
+    return HttpResponse(template.render())
+
+def onlineExam(request):
+    template = loader.get_template('trainee/onlineExam.html')
+    return HttpResponse(template.render())
+
+def discussionForm(request):
+    template = loader.get_template('trainee/discussionForm.html')
+    return HttpResponse(template.render())
+
+# staff resources
+
+def darpan(request):
+    template = loader.get_template('staff/darpan.html')
+    return HttpResponse(template.render())
+
+def gyandeep(request):
+    template = loader.get_template('staff/gyandeep.html')
+    return HttpResponse(template.render())
+
+def sop(request):
+    template = loader.get_template('staff/sop.html')
+    return HttpResponse(template.render())
+
+# research
+
+def coeKavach(request):
+    template = loader.get_template('research/coeKavach.html')
+    return HttpResponse(template.render())
+
+def _5g(request):
+    template = loader.get_template('research/_5g.html')
+    return HttpResponse(template.render())
+
+def faculty(request):
+    template = loader.get_template('research/faculty.html')
+    return HttpResponse(template.render())
+
+def mous(request):
+    template = loader.get_template('home/mous.html')
+    return HttpResponse(template.render())
+
+def innovations(request):
+    template = loader.get_template('research/innovations.html')
+    return HttpResponse(template.render())
+
+# notification
+
+def selections(request):
+    template = loader.get_template('notifications/selections.html')
+    return HttpResponse(template.render())
+
+def notifications(request):
+    template = loader.get_template('notifications/notifications.html')
+    return HttpResponse(template.render())
+
+def railwaySites(request):
+    template = loader.get_template('notifications/railwaySites.html')
+    return HttpResponse(template.render())
+
+# contact
+
+def reaching(request):
+    template = loader.get_template('contact/reaching.html')
+    return HttpResponse(template.render())
+
+def telephone(request):
+    template = loader.get_template('contact/telephone.html')
+    return HttpResponse(template.render())
+
+def disclaimer(request):
+    template = loader.get_template('contact/disclaimer.html')
+    return HttpResponse(template.render())
+
+def privacyPolicy(request):
+    template = loader.get_template('contact/privacyPolicy.html')
+    return HttpResponse(template.render())
+
+def sexualHarassment(request):
+    template = loader.get_template('contact/sexualHarassment.html')
+    return HttpResponse(template.render())
+
+def rti(request):
+    template = loader.get_template('contact/rti.html')
+    return HttpResponse(template.render())
+
+def nonRailway(request):
+    template = loader.get_template('contact/nonRailway.html')
+    return HttpResponse(template.render())
+
+def links(request):
+    template = loader.get_template('contact/links.html')
+    return HttpResponse(template.render())
